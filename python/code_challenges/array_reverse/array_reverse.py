d49ae6f8eea914b246a39d178542d16aab2f2717
Faisal Abuzaid <faisalabuzaid@outlook.com>
2021-05-03 04:44:47 +0300

    finishChallenge1

--- python/code_challenges/array_reverse/array_reverse.py
+++ python/code_challenges/array_reverse/array_reverse.py
@@ -2,8 +2,7 @@ user_array=list(input().split(","))
 result = []
 
 def reverseArray(list):
-  for i in range(len(list)):
-    result.insert(0, list.pop(0))
+  result = user_array[::-1]
   return(result)
 
 print(reverseArray(user_array))
