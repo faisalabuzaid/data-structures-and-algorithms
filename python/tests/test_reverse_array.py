c90fd6b81616e5f878b7a881931259d8c85ddb88
Faisal Abuzaid <faisalabuzaid@outlook.com>
2021-05-03 13:13:46 +0300

    codechallenge1

--- python/tests/test_reverse_array.py
+++ python/tests/test_reverse_array.py
@@ -2,8 +2,9 @@ from code_challenges.array_reverse.array_reverse import reverseArray
 
 def test_array():
 
-    except1 = [6, 5, 4, 3, 2, 1]
-    except2 = [-12, 823, -923, 10, 23, 3546, 2354, 89]
-
-    assert reverseArray([1, 2, 3, 4, 5, 6]) == except1
-    assert reverseArray([89, 2354, 3546, 23, 10, -923, 823, -12]) == except2
+    except1 = [5, 4, 3, 2, 1]
+    except2 = [-1, 844, 12, 0, 2, 51, 1590, 77]
+    actual1 = reverseArray([1, 2, 3, 4, 5])
+    actual2 = reverseArray([77, 1590, 51, 2, 0, 12, 844, -1])
+    assert  actual1 == except1
+    assert  actual2 == except2
