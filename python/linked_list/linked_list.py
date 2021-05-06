5c5b271cbf533b3739d463829e533d0618c09af2
Faisal Abuzaid <faisalabuzaid@outlook.com>
2021-05-06 17:57:57 +0300

    finishThur

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,61 @@
-class LinkedList:
-    """
-    Put docstring here
-    """
+class Node:
 
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+  # use a magic method so when you print node you see it's value
+    def __str__(self):
+        return self.value
+
+# Define linked list
+class LinkedList():
+
+    # Constructor
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
+    # define my insert method
+    def insert (self, data):
+        new_node = Node(data)
+    # Once we have a head 
+        if self.head :
+            # set our current pointer to the head
+            current = self.head 
+            # while there is a following node that's not None
+            while (current.next):
+                # move to the next node
+                current = current.next
+      
+                # Assign new_node to current.next
+            current.next = new_node
+        else:
+            #Assign new_node to self.head 
+            self.head = new_node
 
-    def some_method(self):
-        # method body here
-        pass
+    def include(self, value):
+        """method to check if the given value is in the linked list"""
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            else:
+                current = current.next
+        return False
+    # __str__ , __repr__
+    def __str__(self):
+        """ returns a string representing all the values in the Linked List, formatted as:
+        "{ a } -> { b } -> { c } -> NULL"
+        """
+        # step 0 - create a new empty string
+        output = ""
+
+        # step 1 iterate over each node
+        current = self.head
+        while current:
+        # step 2 - append each value to the string
+            output += f"{{ {current.value} }} -> "
+            # step 2b:  move to the next item
+            current = current.next
+        output += " Null"
+
+        # step 3 - return the final string
+        return output
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,47 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import LinkedList, Node
+
+def test_empty_linked_list():
+    linked_list = LinkedList()
+    assert linked_list.head == None
+
+def test_Node_created():
+    assert "f" == Node('f').value
+    assert None == Node('f').next
+
+def test_insert_to_empty():
+    val = 'test'
+    linked_list = LinkedList()
+    linked_list.insert(val)
+    assert linked_list.head.value == 'test'
+
+def test_head_to_the_first_el():
+   linked_list = LinkedList()
+   linked_list.insert('a')
+   assert linked_list.head.value == 'a'
+   linked_list.insert('b')
+   assert linked_list.head.value == 'a'
+   linked_list.insert('c')
+   assert linked_list.head.value == 'a'
+   linked_list.insert('d')
+   assert linked_list.head.value == 'a'
+   assert linked_list.head.next.value == 'b'
 
 
-def test_import():
-    assert LinkedList
+# creating a Linked list to test the include method
+linked_list = LinkedList()
+new_nodes = ['I', 'am', 'here', 'and', ['a', 'l', 'i', 7, 3]]
+for node in new_nodes:
+    linked_list.insert(node)
+
+def test_includes_true():
+    assert linked_list.include('here') == True
+    assert linked_list.include(['a', 'l', 'i', 7, 3]) == True
+    assert linked_list.include('am') == True
+
+def test_includes_false():
+    assert linked_list.include('fff') == False
+    assert linked_list.include([]) == False
+    assert linked_list.include(4) == False
+
+def test__str__method():
+    assert linked_list.__str__() == "{ I } -> { am } -> { here } -> { and } -> { ['a', 'l', 'i', 7, 3] } ->  Null"
