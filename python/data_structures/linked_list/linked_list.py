3f4831222dfb67d55c801f29c14f9691331c2b71
Faisal Abuzaid <faisalabuzaid@outlook.com>
2021-05-18 14:21:13 +0300

    firstcommit

--- python/data_structures/linked_list/linked_list.py
+++ python/data_structures/linked_list/linked_list.py
@@ -1,3 +1,4 @@
+
 class Node:
 
     def __init__(self, value=None, next=None):
@@ -28,8 +29,33 @@ class LinkedList():
             else:
                 current = current.next
         return False
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            current = last.next
+        last.next = new_node
+
+
+
+    def insert_after(self, value, new_data):
+        if self.include(value):
+            current = self.head
+            while current:
+                if current.value == value:
+                    node = Node(new_data)
+                    node.next = current.next
+                    current.next = node
+                    return self.__str__()
+                current = current.next
+        else:
+            return 'Value is not in the list'
+    
         
-    def insertBefore(self, search_value, new_value):
+    def insert_before(self, search_value, new_value):
         if self.head == None:
           return "The value is not exist"
 
@@ -85,3 +111,25 @@ class LinkedList():
 
         # step 3 - return the final string
         return output
+
+test = LinkedList()
+test.insert(1)
+print(test)
+test.append(2)
+print(test)
+test.insert_after(1,10)
+print(test)
+test.insert_before(2, 4)
+print(test)
+
+def linked_list():
+    linked_list = LinkedList()
+    new_nodes = ['I', 'am', 'here', 'and', ['a', 'l', 'i', 7, 3]]
+    for node in new_nodes:
+        linked_list.insert(node)
+    return linked_list
+
+# print(linked_list())
+# test=linked_list()
+# test.insert_before("and",3)
+# print(test)
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,5 @@
+from data_structures.linked_list.linked_list import LinkedList, Node
 import pytest
-from code_challenges.data_structures.linked_list.linked_list import LinkedList, Node
 
 def test_empty_linked_list():
     linked_list = LinkedList()
@@ -63,3 +63,47 @@ def test_kthFromEnd2(linked_list):
     print(actual)
     excpected = 'Sorry, the value is larger than the linked list'
     assert excpected == actual
+
+# def test_append():
+#     assert "d, 4, b, a, z" == linked_list.append('z')
+
+# def test_append_to_empty():
+#     assert "z" == linked_list.append('z')
+
+# def test_insert_before_value_in_list():
+#     assert "d, 4, b, 3, a, z" == linked_list.insert_before('a', 3)
+
+# def test_insert_before_value_not_in_list():
+
+#     assert 'Value is not in the list' == linked_list.insert_before('e', 3)
+
+# def test_insert_before_to_empty_list(linked_list):
+#     assert 'Value is not in the list' == linked_list.insert_before('e', 3)
+
+# def test_insert_after_value_in_list(linked_list):
+
+#     assert "d, 4, b, 3, cat, a, z" == linked_list.insert_after(3, 'cat')
+
+# def test_insert_after_value_not_in_list(linked_list):
+
+#     assert 'Value is not in the list' == linked_list.insert_after('e', 3)
+
+# def test_insert_after_to_empty_list(linked_list):
+#     assert 'Value is not in the list' == linked_list.insert_before('e', 3)
+
+# def test_append_2(linked_list):
+#     assert "d, 4, b, 3, cat, a, z, z2" == linked_list.append('z2')
+#     assert "d, 4, b, 3, cat, a, z, z2, e2" == linked_list.append('e2')
+
+# def test_insert_before_node_in_the_middle(linked_list):
+#     assert "d, 4, b, 3, 123, cat, a, z, z2, e2" == linked_list.insert_before('cat', 123)
+
+# def test_insert_before_first_node(linked_list):
+#     assert "new_first, d, 4, b, 3, 123, cat, a, z, z2, e2" == linked_list.insert_before('d', "new_first")
+
+
+# def test_insert_after_the_middle(linked_list):
+#     assert "new_first, d, 4, b, 3, 123, cat, dog, a, z, z2, e2" == linked_list.insert_after('cat', 'dog')
+
+# def test_insert_after_the_last(linked_list):
+#     assert "new_first, d, 4, b, 3, 123, cat, dog, a, z, z2, e2, 1000" == linked_list.insert_after('e2', '1000')
